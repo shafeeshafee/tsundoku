94eed960b1c92c6320228e19a5c50061a1a7f510
Shan Siddiqui <shansiddiqui@Shans-Air.lan>
2021-03-10 17:28:33 -0500

    updated footer1

--- src/App.js
+++ src/App.js
@@ -1,39 +1,48 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Redirect, NavLink } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  NavLink,
+} from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import AboutUs from "./Pages/AboutUs";
 import ErrorPage from "./Pages/ErrorPage";
 import BestSellers from "./Pages/BestSellers";
 import SearchBooks from "./Pages/SearchBooks";
-
+import Footer from "./Components/Footer";
 function App() {
-	return (
-		<Router>
-			<nav>
-				<div>
-				<NavLink to="/">Home</NavLink>
-				<NavLink to="/bestsellers">Best Sellers</NavLink>
-				<NavLink to="/aboutus">About Us</NavLink>
-				<NavLink to="/search">Search</NavLink>
-				</div>
-			</nav>
-			<Switch>
-				<Route exact path="/">
-					<HomePage />
-				</Route>
-				<Route exact path="/bestsellers">
-					<BestSellers />
-				</Route>
-				<Route exact path="/aboutus">
-					<AboutUs />
-				</Route>
-				<Route exact path="/search">
-					<SearchBooks />
-				</Route>
-				<Redirect to="/" component={ErrorPage} />
-			</Switch>
-    	</Router>
-	)
+  return (
+    <>
+      <Router>
+        <nav>
+          <div>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/bestsellers">Best Sellers</NavLink>
+            <NavLink to="/aboutus">About Us</NavLink>
+            <NavLink to="/search">Search</NavLink>
+          </div>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/bestsellers">
+            <BestSellers />
+          </Route>
+          <Route exact path="/aboutus">
+            <AboutUs />
+          </Route>
+          <Route exact path="/search">
+            <SearchBooks />
+          </Route>
+          <Redirect to="/" component={ErrorPage} />
+        </Switch>
+      </Router>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
