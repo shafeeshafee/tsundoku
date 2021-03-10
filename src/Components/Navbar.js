bf3066ca5f61af62449899cdd0dbcc5a94d88174
Shafee <shafeelinks@gmail.com>
2021-03-09 20:51:19 -0500

    navbar padding has been fixed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
 	return (
-		<nav className="flex justify-evenly">
-			<div className="font-headings flex justify-around w-screen bg-offwhite">
+		<nav className="flex justify-evenly bg-offwhite">
+			<div className="font-headings flex justify-around w-screen bg-offwhite text-2xl">
 				<NavLink to="/">home</NavLink>
 				<NavLink to="/bestsellers">best sellers</NavLink>
 				<NavLink to="/aboutus">about us</NavLink>
