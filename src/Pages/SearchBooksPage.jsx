4a7bb851f69b914080f6245f67a6189eeca7286a
Shafee <shafeelinks@gmail.com>
2021-03-10 23:50:34 -0500

    styled search page with grid and even spacing between text and image

--- src/Pages/SearchBooksPage.jsx
+++ src/Pages/SearchBooksPage.jsx
@@ -1,34 +1,34 @@
-import {useEffect} from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import {fetchBooks} from '../Actions/booksActions';
-import SearchBooks from '../Components/SearchBooks'
-import SearchResults from '../Components/SearchResults'
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchBooks } from "../Actions/booksActions";
+import SearchBooks from "../Components/SearchBooks";
+import SearchResults from "../Components/SearchResults";
 
 const SearchBooksPage = () => {
-  const dispatch = useDispatch()
+	const dispatch = useDispatch();
 
-  const initialBooks = useSelector((state) => {
-    return state.books
-  })
+	const initialBooks = useSelector((state) => {
+		return state.books;
+	});
 
-  const {books} = initialBooks;
+	const { books } = initialBooks;
 
-  useEffect(() => {
-    dispatch(fetchBooks('cat'))
-  }, [dispatch])
+	useEffect(() => {
+		dispatch(fetchBooks("cat"));
+	}, [dispatch]);
 
-  const renderBooks = () => {
-    if (books.loading) return <p> Loading BestSellers...</p>
-    if (books.hasErrors) return <p>An error has occurred</p>
-    return books.items;
-  }
-  
-  return (
-    <div>
-      <SearchBooks />
-      <SearchResults bookslist={renderBooks()}/>
-    </div>
-  )
-}
+	const renderBooks = () => {
+		if (books.loading) return <p> Loading BestSellers...</p>;
+		if (books.hasErrors) return <p>An error has occurred</p>;
+		return books.items;
+	};
+
+	return (
+		<div className="bg-offwhite flex flex-col font-headings">
+			<SearchBooks />
+			<SearchResults bookslist={renderBooks()} />
+		</div>
+	);
+};
 
 export default SearchBooksPage;
