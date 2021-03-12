f629e758436cab6fbc1c7d06958edc3de84f92ae
Jahaziel <blaquebeardcoder@gmail.com>
2021-03-12 09:58:47 -0500

    created mission page template

---
+++ src/Pages/Mission.js
@@ -0,0 +1,19 @@
+import React from 'react'
+//Mission Page
+function Mission() {
+    return (
+        <div>
+            <h1>Our Mission</h1>
+        </div>
+    );
+}
+
+export default Mission;
+
+/**
+ * Reference medium article layout
+ * Share stories from around the web of WHY reading is here to stay and important
+ * Links to indie bookstores
+ * Anchor tags with links to other articles
+ * Reference "Anti Library: having the option" 
+ */
