fd67088052f4eb62e82cb0ef80d206af4641728f
Shafee <shafeelinks@gmail.com>
2021-03-13 00:02:22 -0500

    footer is more fat now

--- src/Components/Bestsellers.jsx
+++ src/Components/Bestsellers.jsx
@@ -7,8 +7,8 @@ const BestSellers = ({ bestsellerslist }) => {
 
 	return (
 		<div className="bg-offwhite font-headings pb-20">
-			<h1 className="text-center pt-10 text-2xl uppercase font-bold">TRENDING FICTION</h1>
-			<ul className="px-20 pt-10 grid-flow-row grid grid-cols-1 gap-20 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5">
+			<h1 className="text-center py-20 text-2xl uppercase font-bold">TRENDING FICTION</h1>
+			<ul className="px-20 grid-flow-row grid grid-cols-1 gap-20 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5">
 				{books?.map((el) => {
 					return <BestSellerSingle {...el} />;
 				})}
