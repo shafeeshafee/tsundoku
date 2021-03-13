800fd68ef6fd5c70a047a7739a5561ca89a9d545
Shan Siddiqui <shansiddiqui@Shans-Air.lan>
2021-03-12 23:02:11 -0500

    updated search bar, addded placeholder, added Tailwind

--- src/Components/SearchBooks.jsx
+++ src/Components/SearchBooks.jsx
@@ -8,7 +8,7 @@ function SearchBooks() {
         className="border-2 border-gray-200 bg-offwhite  w-56 h-10 rounded-sm px-3 pl-8 py-1  rounded-lg text-sm focus:outline-none"
         // className={"border-b-2 bg-offwhite md:w-full px-8"}
         type="text"
-        placeholder="Title / Author / ISBN"
+        placeholder="Title / Author / Topic"
       />
     </div>
   );
