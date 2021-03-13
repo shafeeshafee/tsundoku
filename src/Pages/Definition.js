17b650f7fe280979e77202e154a097d3918c5f6f
Shafee <shafeelinks@gmail.com>
2021-03-13 01:18:27 -0500

    got rid of the third panel to add it as a seperate component

--- src/Pages/Definition.js
+++ src/Pages/Definition.js
@@ -3,33 +3,17 @@ import ScrollAnimation from "react-animate-on-scroll";
 const Definition = () => {
 	return (
 		<ScrollAnimation animateIn="fadeIn">
-			<div className="flex h-screen font-headings">
+			<div className="flex h-screen font-title">
 				<div className="bg-dark w-full flex flex-col justify-evenly text-offwhite">
 					{/* first panel */}
-					<div className="w-3/4 m-auto flex flex-col justify-around text-4xl">
-						<h1 className="font-bold mb-10">tsundoku (n.)</h1>
-						<p className="font-light">the act of acquiring books that then pile up, often unread, in one's home</p>
+					<div className="w-3/4 m-auto flex flex-col justify-around">
+						<h1 className="font-bold text-base lg:text-4xl mb-10">tsundoku (n.)</h1>
+						<p className="font-light text-base lg:text-4xl">the act of acquiring books that then pile up, often unread, in one's home</p>
 					</div>
 				</div>
 				{/* second panel */}
 				<div className="hidden lg:block bg-girl-library w-full"></div>
 				{/* third panel */}
-				<div className="w-full center-content font-bold text-6xl leading-9 border-t-4 border-b-4">
-					<p>read more</p>
-					<p>read more</p>
-					<p>read more</p>
-					<p>read more</p>
-					<p>read more</p>
-					<p>read more</p>
-					<p>read more</p>
-					<p>read more</p>
-					<p>read more</p>
-					<p>read more</p>
-					<p>read more</p>
-					<p className="text-reddish z-30">read more</p>
-					<p>read more</p>
-					<p>read more</p>
-				</div>
 			</div>
 		</ScrollAnimation>
 	);
