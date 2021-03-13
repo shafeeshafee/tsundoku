16aadb11546b3bb5d8995fcdc0628eebe97f91a6
Shafee <shafeelinks@gmail.com>
2021-03-13 00:05:22 -0500

    bestsellers and logo animations are added to the page

--- src/Components/Bestsellers.jsx
+++ src/Components/Bestsellers.jsx
@@ -1,4 +1,5 @@
 import BestSellerSingle from "./BestSellerSingle";
+import ScrollAnimation from "react-animate-on-scroll";
 
 const BestSellers = ({ bestsellerslist }) => {
 	const { books } = bestsellerslist;
@@ -6,14 +7,16 @@ const BestSellers = ({ bestsellerslist }) => {
 	console.log(books);
 
 	return (
-		<div className="bg-offwhite font-headings pb-20">
-			<h1 className="text-center py-20 text-2xl uppercase font-bold">TRENDING FICTION</h1>
-			<ul className="px-20 grid-flow-row grid grid-cols-1 gap-20 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5">
-				{books?.map((el) => {
-					return <BestSellerSingle {...el} />;
-				})}
-			</ul>
-		</div>
+		<ScrollAnimation duration={1} animateIn="fadeIn">
+			<div className="bg-offwhite font-headings pb-20">
+				<h1 className="text-center py-20 text-2xl uppercase font-bold">TRENDING FICTION</h1>
+				<ul className="px-20 grid-flow-row grid grid-cols-1 gap-20 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5">
+					{books?.map((el) => {
+						return <BestSellerSingle {...el} />;
+					})}
+				</ul>
+			</div>
+		</ScrollAnimation>
 	);
 };
 
