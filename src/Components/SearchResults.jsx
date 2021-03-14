8d8ca8fc4281bcb63c9b2cc83a948a88957a25e9
Shafee <shafeelinks@gmail.com>
2021-03-14 01:22:54 -0500

    added full height instead of screen height for search results on mobile

--- src/Components/SearchResults.jsx
+++ src/Components/SearchResults.jsx
@@ -2,19 +2,19 @@ import SingleBook from "./SingleBook";
 import ScrollAnimation from "react-animate-on-scroll";
 
 function SearchResults({ bookslist }) {
-  return (
-    <ScrollAnimation animateIn="fadeIn">
-      <div className="px-20 mt-10 grid grid-cols-1 gap-20 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5">
-        {bookslist?.map((el) => {
-          return <SingleBook bookInfo={el} key={el.id} />;
-        })}
-        :
-        <div>
-          <p>Quotes</p>
-        </div>
-      </div>
-    </ScrollAnimation>
-  );
+	return (
+		<ScrollAnimation animateIn="fadeIn">
+			<div className="px-20 mt-10 grid grid-cols-1 gap-20 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5">
+				{bookslist?.map((el) => {
+					return <SingleBook bookInfo={el} key={el.id} />;
+				})}
+				:
+				<div>
+					<p>Quotes</p>
+				</div>
+			</div>
+		</ScrollAnimation>
+	);
 }
 
 export default SearchResults;
