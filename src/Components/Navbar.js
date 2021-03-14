9de7544fbb5677543540373c844fb2366ce8693b
Shafee <shafeelinks@gmail.com>
2021-03-13 22:09:39 -0500

    hamburger

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,40 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ toggle, totalItems }) => {
 	return (
-		<nav className="flex justify-evenly">
-			<div className="pt-5 font-headings flex justify-evenly w-screen bg-offwhite text-lg">
-				<NavLink to="/">home</NavLink>
-				<NavLink to="/bestsellers">best sellers</NavLink>
-				<NavLink to="/aboutus">about us</NavLink>
-				<NavLink to="/search">search</NavLink>
+		<nav
+			className="font-headings bg-offwhite z-40 top-0 flex justify-between items-center h-16 text-black shadow-sm header-font "
+			role="navigation"
+		>
+			<Link to="/" className="flex items-center pl-8">
+				<img alt="" className="h-10 mx-2 " />
+				tsundoku
+			</Link>
+			<div className="px-4 cursor-pointer md:hidden" onClick={toggle}>
+				<svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+					<path
+						fillRule="evenodd"
+						d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+						clipRule="evenodd"
+					/>
+				</svg>
+			</div>
+			<div className="pr-8 xl:flex md:block hidden">
+				<Link className="hover:bg-secondary p-4 lowercase" to="/">
+					Home
+				</Link>
+				<Link className="hover:bg-secondary p-4 lowercase" to="/bestsellers">
+					bestsellers
+				</Link>
+				<Link className="hover:bg-secondary p-4 lowercase" to="/aboutus">
+					about us
+				</Link>
+				<Link className="hover:bg-secondary p-4 lowercase" to="/mission">
+					mission
+				</Link>
+				<Link className="hover:bg-secondary p-4 lowercase" to="/search">
+					search
+				</Link>
 			</div>
 		</nav>
 	);
