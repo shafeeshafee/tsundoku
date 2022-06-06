6220ccff7271a30043b5514ff49458ba2ebfd9ea
Shafee <shafeelinks@gmail.com>
2022-06-06 10:36:16 -0400

    remove console.log from code

--- src/Components/Bestsellers.jsx
+++ src/Components/Bestsellers.jsx
@@ -1,20 +1,20 @@
 import BestSellerSingle from "./BestSellerSingle";
 
 const BestSellers = ({ bestsellerslist }) => {
-	const { books } = bestsellerslist;
+  const { books } = bestsellerslist;
 
-	console.log(books);
-
-	return (
-		<div className="bg-offwhite font-headings pb-20">
-			<h1 className="text-center py-20 text-2xl uppercase font-bold">Trending</h1>
-			<ul className="px-20 grid-flow-row grid grid-cols-1 gap-20 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5">
-				{books?.map((el) => {
-					return <BestSellerSingle {...el} />;
-				})}
-			</ul>
-		</div>
-	);
+  return (
+    <div className="bg-offwhite font-headings pb-20">
+      <h1 className="text-center py-20 text-2xl uppercase font-bold">
+        Trending
+      </h1>
+      <ul className="px-20 grid-flow-row grid grid-cols-1 gap-20 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5">
+        {books?.map((el) => {
+          return <BestSellerSingle {...el} />;
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default BestSellers;
